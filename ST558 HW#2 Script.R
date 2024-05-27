8b26ba8535f9f9cedb8664348eff2101e5660359
l-flaherty <lflaherty1@alum.babson.edu>
2024-05-27 01:19:29 -0400

    First draft

---
+++ ST558 HW#2 Script.R
@@ -0,0 +1,125 @@
+##########Written By Liam Flaherty For ST558 HW2###########
+####still need to fix knitr in pt 2, and summary in pt 4###
+
+
+#####1. Basic Vector Practice#####
+pre_bp=c(130, 128, 116, 124, 133,                    #data provided#
+         134, 118, 126, 114, 127,
+         141, 138, 128, 140, 137,
+         131, 120, 128, 139, 135)
+post_bp=c(114, 98, 113, 99, 107,                     #data provided#
+          116, 113, 111, 119, 117,
+          101, 119, 130, 122, 106,
+          106, 124, 102, 117, 113)
+
+names(pre_bp)=paste0("Subject_", 1:length(pre_bp))   #add names attribute#
+names(post_bp)=paste0("Subject_", 1:length(post_bp)) #add names attribute#
+ 
+diff_bp=post_bp-pre_bp                               #new vector for the difference#
+mean(diff_bp)                                        #find the mean of the difference#
+
+which(diff_bp<0)                                     #subjects who experienced decrease# 
+mean(diff_bp[which(diff_bp<0)])                      #mean difference of those who decreased#
+
+
+
+
+
+#####2. Basic Data Frame Practice#####
+treatment=data.frame(                                #Put everything into a dataframe#
+  patient=names(pre_bp),
+  pre_bp, 
+  post_bp,
+  diff_bp)
+
+treatment[which(treatment$diff_bp<0),]               #Filter the df to those who decreased#
+
+treatment$low_bp=ifelse(treatment$post_bp<120, TRUE, FALSE)
+#knitr::kable(treatment)#
+
+
+
+
+
+#####3. List Practice#####
+pre_bp=c(138, 135, 147, 117, 152,                      #overwriting previous vector#
+         134, 114, 121, 131, 130)
+post_bp=c(105, 136, 123, 130, 134,                     #overwriting previous vector#
+          143, 135, 139, 120, 124)
+
+placebo=data.frame(
+  patient=paste0("Placebo_Subject_", 1:length(pre_bp)),
+  pre_bp,
+  post_bp,
+  diff_bp=post_bp-pre_bp,                              #add difference variable directly to df#
+  low_bp=ifelse(post_bp<120, TRUE, FALSE))             #add variable for low bp after treatment#
+
+mylist=list(treatment=treatment, placebo=placebo)      #Place the df's together#
+
+mylist[1]                                              #returns a list#
+mylist[[1]]                                            #returns a dataframe#
+mylist["treatment"]                                    #other way to access df in list#
+mylist[-2]                                             #even more ways to access df in list#
+
+mylist[["placebo"]]["pre_bp"]                          #access specific part of df from list#
+
+
+
+
+
+#####4. Control Flow Practice#####
+treatment$status=character(nrow(treatment))            #initialize#
+placebo$status=character(nrow(placebo))                #initialize#
+
+for(i in 1:nrow(mylist[["treatment"]])) {              #just to practice 'for'#  
+  a=mylist[["treatment"]][["post_bp"]]                 #to make less cluttered#
+  if(a[i]>130) {                                       #note if data bigger, wouldn't...#
+    status[i]=="high"                                  #...want to keep calling object...#
+  } else if (a[i]>120) {                               #for performance reasons#
+    status[i]=="borderline"
+  } else {
+    status[i]=="optimal"
+  }
+}
+
+for(i in 1:nrow(mylist[["placebo"]])) {                #just to practice 'for'#
+  a=mylist[["placebo"]][["post_bp_placebo"]]           #to make less cluttered#
+  if(a[i]>130) {      
+    status[i]=="high"
+  } else if (a[i]>120) {
+    status[i]=="borderline"
+  } else {
+    status[i]=="optimal"
+  }
+}
+
+
+
+
+
+#####5. Function Writing#####
+myfunction=function(l, func=mean) {       #set default function to mean#
+  df1=l[[1]]; df2=l[[2]]
+  a=c("pre_bp", "post_bp", "diff_bp")     #variables to select, assume shared names#
+  b=vector()                              #initialize#
+  
+  for (i in 1:length(a)) {                #apply function (no l/sappy yet)#
+    b[(2*i)-1]=func(df1[,a[i]])
+    b[(2*i)]=func(df2[,a[i]])
+  }
+  
+  stat=deparse(substitute(func))          #get name of user inputted stat#
+  
+  names(b)=paste0(
+    rep(c(names(l)), length(a)),          #names of df's in list#
+    "_", rep(a,2), "_",                   #names of columns in df#
+     stat)                                #name of desired stat#
+                
+return(b)                                 #directions say named vector instead of list okay#
+}
+
+myfunction(mylist)
+myfunction(mylist, var)
+myfunction(mylist, sd)
+myfunction(mylist, min)
+myfunction(mylist, max)
